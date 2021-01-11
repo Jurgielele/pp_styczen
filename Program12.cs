db2074b5d37dcd8c631ba5a9e430ef2dc8f2bee5
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2021-01-11 06:59:52 +0100

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oct = "";
+            Console.WriteLine("Podaj n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n>0)
+            {
+                oct = n % 8 + oct;
+                n /= 8;
+            }
+            Console.WriteLine(oct);
+        }
+
+    }
+}
