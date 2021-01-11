43d97efc07e4325a554254f91621875c5332faf2
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2021-01-11 06:52:26 +0100

    Add files via upload

---
+++ Program13.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace program13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hex = "";
+            Console.WriteLine("Podaj n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                if ((n % 16) < 10) hex = n % 16 + hex;
+                else
+                {
+                    string a = "";
+
+                    switch (n % 16)
+                    {
+                        case 10:
+                            a = "A";
+                            break;
+                        case 11:
+                            a = "B";
+                            break;
+                        case 12:
+                            a = "C";
+                            break;
+                        case 13:
+                            a = "D";
+                            break;
+                        case 14:
+                            a = "E";
+                            break;
+                        case 15:
+                            a = "F";
+                            break;
+                    }
+                    hex = a + hex;
+                }
+                n /= 16;
+            }
+            Console.WriteLine(hex);
+        }
+
+    }
+}
