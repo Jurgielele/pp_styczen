3c9f7469eea16eac7e61e5d4b3f1f0996ee94303
Jurgielele <73314712+Jurgielele@users.noreply.github.com>
2021-01-11 06:30:02 +0100

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace program10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            Console.WriteLine("Podaj 1<=n<=1000");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] tab = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Podaj elementy");
+                tab[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(tab);
+            foreach (int element in tab)
+            {
+                sum += element * element;
+            }
+            Console.WriteLine("Sum: " + sum);
+
+
+        }
+    }
+}
